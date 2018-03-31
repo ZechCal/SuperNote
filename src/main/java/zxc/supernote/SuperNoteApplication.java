9bbb2fbefc63200324d834d4dfcf2d66181389ea
ZechCal <zxyworkspace@outlook.com>
2018-03-31 21:23:48 +0800

    first commit

---
+++ src/main/java/zxc/supernote/SuperNoteApplication.java
@@ -0,0 +1,12 @@
+package zxc.supernote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperNoteApplication.class, args);
+	}
+}
